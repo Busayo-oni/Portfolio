43686a9e79ca141025f4355a70538fb122bc6108
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2025-01-21 21:33:44 +0100

    center text alignment for address in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className='flex gap-2 flex-col items-center lg:items-start test '>
             <p className='text-gray-600 lg:text-lg text-sm '><FaMailBulk className='inline-block mr-2' /> busayaooni@gmail.com</p> 
             <p className='text-gray-600 lg:text-lg text-sm'><FaWhatsapp className='inline-block mr-2' /> +234 8155 247 644</p> 
-            <p className='text-gray-600 lg:text-lg text-sm'><FaLocationArrow className='inline-block mr-2' /> No.11, Adejokun Street, Isheri Ojodu Berger, Lagos State.</p> 
+            <p className='text-gray-600 text-center lg:text-lg text-sm'><FaLocationArrow className='inline-block mr-2' /> No.11, Adejokun Street, Isheri Ojodu Berger, Lagos State.</p> 
             </div>
             </div>
     </div>
